f08cded24aa5becd3b2a5dc2c21b1983cef046b2
webalexeu <28548335+webalexeu@users.noreply.github.com>
2024-11-04 14:43:33 +0100

    [Feature] Add authentication method
    * Add new authentication method parameter to support multiple API authentication mechanisms

---
+++ examples/General.ps1
@@ -0,0 +1,7 @@
+<#
+  .SYNOPSIS
+    This is a general example of how to use the module.
+#>
+
+# Import the module
+Import-Module -Name 'PSWinBGP'
--- tests/PSWinBGP.Tests.ps1
+++ tests/PSWinBGP.Tests.ps1
@@ -1,11 +1,20 @@
-﻿[CmdletBinding()]
-Param(
-    # Path to the module to test.
-    [Parameter()]
-    [string] $Path
-)
-
-Write-Verbose "Path to the module: [$Path]" -Verbose
-
-Describe 'PSWinBGP' {
+﻿Describe 'PSWinBGP' {
+    It 'Function: Get-WinBGPRoute' {
+        Get-WinBGPRoute -ComputerName localhost | Should -Be "@{Result=API connection timeout}"
+    }
+    It 'Function: Start-WinBGPRoute' {
+        Start-WinBGPRoute -ComputerName localhost -RouteName 'winbgp.contoso.com' | Should -Be "@{Result=API connection timeout}"
+    }
+    It 'Function: Stop-WinBGPRoute' {
+        Stop-WinBGPRoute -ComputerName localhost -RouteName 'winbgp.contoso.com' | Should -Be "@{Result=API connection timeout}"
+    }
+    It 'Function: Start-WinBGPRouteMaintenance' {
+        Start-WinBGPRouteMaintenance -ComputerName localhost -RouteName 'winbgp.contoso.com' | Should -Be "@{Result=API connection timeout}"
+    }
+    It 'Function: Stop-WinBGPRouteMaintenance' {
+        Stop-WinBGPRouteMaintenance -ComputerName localhost -RouteName 'winbgp.contoso.com' | Should -Be "@{Result=API connection timeout}"
+    }
+    It 'Function: Set-PSWinBGPConfig' {
+        Set-PSWinBGPConfig | Should -BeNullOrEmpty
+    }
 }
