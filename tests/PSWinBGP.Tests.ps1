d572f418dc3475213346726a8490173475ede18e
WebalexEU <28548335+webalexeu@users.noreply.github.com>
2024-11-19 11:49:16 +0100

    fix tests

--- tests/PSWinBGP.Tests.ps1
+++ tests/PSWinBGP.Tests.ps1
@@ -3,10 +3,10 @@
         Get-WinBGPRoute -ComputerName localhost | Should -Be "@{Result=API connection timeout}"
     }
     It 'Function: Start-WinBGPRoute' {
-        Start-WinBGPRoute -ComputerName localhost -Name 'winbgp.contoso.com' | Should -Be "@{Result=API connection timeout}"
+        Start-WinBGPRoute -ComputerName localhost -Name 'winbgp.contoso.com' -Confirm:$false | Should -Be "@{Result=API connection timeout}"
     }
     It 'Function: Stop-WinBGPRoute' {
-        Stop-WinBGPRoute -ComputerName localhost -Name 'winbgp.contoso.com' | Should -Be "@{Result=API connection timeout}"
+        Stop-WinBGPRoute -ComputerName localhost -Name 'winbgp.contoso.com' -Confirm:$false | Should -Be "@{Result=API connection timeout}"
     }
     It 'Function: Start-WinBGPRouteMaintenance' {
         Start-WinBGPRouteMaintenance -ComputerName localhost -Name 'winbgp.contoso.com' | Should -Be "@{Result=API connection timeout}"
